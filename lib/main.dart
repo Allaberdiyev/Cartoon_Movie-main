f5a74c3d0e418a46f1c2c8327f4bd20bd960fded
Allaberdiyev <149064535+Allaberdiyev@users.noreply.github.com>
2024-05-11 18:40:25 +0500

    CARTON

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:carton/pages/paje1.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:carton/pages/paje1.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SearchPage(),
+      ),
+    );
+  }
+}
---
+++ lib/pages/paje2.dart
@@ -0,0 +1,283 @@
+import 'package:carton/pages/page3.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:glassmorphism/glassmorphism.dart';
+import 'package:carton/pages/page3.dart';
+
+class ResultSearchPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 0, 0, 0),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 15.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: 45.h,
+                  child: TextField(
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 0.5),
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.all(20),
+                      filled: true,
+                      fillColor: const Color(0xFF37324B),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none,
+                      ),
+                      hintText: "Comedy",
+                      hintStyle: TextStyle(
+                        color: const Color(0xFFAAAAB9),
+                        fontSize: 16.sp,
+                        letterSpacing: 0.5,
+                        fontWeight: FontWeight.w400,
+                        decorationThickness: 6,
+                      ),
+                      suffixIcon: Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 10.w, vertical: 5.h),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.clear),
+                        ),
+                      ),
+                      suffixIconColor: const Color(0xFFAAAAB9),
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.only(left: 25.w, right: 15.w),
+                        child: const Icon(
+                          CupertinoIcons.search,
+                          size: 30,
+                        ),
+                      ),
+                      prefixIconColor: const Color(0xFFAAAAB9),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 25.h,
+                ),
+                const Text(
+                  "Result for \"comedy\"",
+                  style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(
+                  height: 15.h,
+                ),
+                Wrap(
+                  spacing: 10.w,
+                  runSpacing: 10.h,
+                  children: [
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/pictures/image.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/pictures/image.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/pictures/image.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/pictures/image.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/pictures/image.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/pictures/image.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/pictures/image.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/pictures/image.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/pictures/image.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/pictures/image.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(15),
+        child: GlassmorphicContainer(
+          width: double.infinity,
+          height: 60.h,
+          alignment: Alignment.center,
+          blur: 20,
+          border: 0,
+          borderRadius: 15,
+          linearGradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                const Color(0xFFffffff).withOpacity(0.1),
+                const Color(0xFFFFFFFF).withOpacity(0.05),
+              ],
+              stops: const [
+                0.1,
+                1,
+              ]),
+          borderGradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              const Color(0xFFffffff).withOpacity(0.5),
+              const Color((0xFFFFFFFF)).withOpacity(0.5),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  CupertinoIcons.home,
+                  size: 30,
+                  color: Colors.white70,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  CupertinoIcons.search,
+                  size: 30,
+                  color: Colors.blue,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  CupertinoIcons.bookmark,
+                  size: 30,
+                  color: Colors.white70,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DownloadPage(),
+                    ),
+                  );
+                },
+                icon: const Icon(
+                  CupertinoIcons.arrow_down_doc,
+                  size: 30,
+                  color: Colors.white70,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  CupertinoIcons.person,
+                  size: 30,
+                  color: Colors.white70,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
